3f06f4c592f5cad807673f123b84473450798023
shaileshyadav7771 <shaileshyadav7958@gmail.com>
2021-11-06 14:32:26 +0530

    Created Store models And admin.py file

--- Bazar/settings.py
+++ Bazar/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'import_export',
     'category',
     'accounts',
+    'store',
 ]
 
 MIDDLEWARE = [
--- category/admin.py
+++ category/admin.py
@@ -7,6 +7,7 @@ class CategoryAdmin(ImportExportModelAdmin):
 	list_display=['id','category_name','slug','description','cat_image']
 	###Adding this line so that we Can Search/filter user result in case of any changes or to Check last time when he updated#######
 	search_fields = ('category_name','slug','description','cat_image')
+	prepopulated_fields = {"slug":('category_name',)}
 
 
 admin.site.register(Category,CategoryAdmin)
