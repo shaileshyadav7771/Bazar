1d095efff2256996fecebf265ae998516c487425
shaileshyadav7771 <shaileshyadav7958@gmail.com>
2021-11-06 15:26:23 +0530

    created views and rendered data to template and made changes in html code.

--- store/admin.py
+++ store/admin.py
@@ -6,10 +6,11 @@ from import_export.admin import ImportExportModelAdmin
 
 
 class ProductAdmin(ImportExportModelAdmin):
-	list_display=['id','product_name','price','images','stock','is_available','category','created_date','modified_date','description',]
+	list_display=['id','product_name','price','images','stock','is_available','category','created_date','modified_date','description']
 	###Adding this line so that we Can Search/filter user result in case of any changes or to Check last time when he updated#######
 	search_fields = ('id','product_name','price','images','stock','is_available','category','created_date','modified_date','description')
-	list_display_links = ('email', 'first_name', 'last_name')
+	list_display_links = ('id','product_name','price','images','stock',)
 	ordering = ('-price',)
+	prepopulated_fields = {"slug": ('product_name',)}
 
 admin.site.register(Product,ProductAdmin)
