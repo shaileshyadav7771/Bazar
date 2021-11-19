8ed382407c0a07bdd2706d620092cf093e5317d0
shaileshyadav7771 <shaileshyadav7958@gmail.com>
2021-11-19 20:46:04 +0530

    Adding Card &  Cart Items Model.

--- Bazar/urls.py
+++ Bazar/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('',views.home , name='home'),
     #let include store url first traffic will come here and then we will handle in store url's :)
     path('store/',include('store.urls')),
+    path('cart/',include('carts.urls')),
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ carts/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def cart(request):
+
+	return render(request , 'store/cart.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
