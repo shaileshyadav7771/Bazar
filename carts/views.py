05b2cfe1e6d86d8d29584608e37b5019ea3811a6
shaileshyadav7771 <shaileshyadav7958@gmail.com>
2021-12-05 22:13:00 +0530

    Adding Card &  Cart Items Model.

--- carts/views.py
+++ carts/views.py
@@ -4,8 +4,11 @@ from django.shortcuts import render
 
 
 def cart(request):
-
 	return render(request , 'store/cart.html')
+
+
+def add_cart(request):
+	product=Product.objects.get(id=product_id)
 
 
 
